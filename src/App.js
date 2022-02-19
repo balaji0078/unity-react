e72c19113f851559807ccb17934e57f27c2225e0
BALAJI E <46436245+balaji0078@users.noreply.github.com>
2022-02-19 17:01:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -404,14 +404,10 @@ function App() {
             <NavLink activeClassName="active" to="/Create">SignIn</NavLink>
            <NavLink activeClassName="active" to="/">Login</NavLink>
            <NavLink activeClassName="active" to="/stores">Stores</NavLink>
-
-//             <a style={{marginLeft:17}} href='https://policywrite.s3.ap-south-1.amazonaws.com/Doc1.docx'>
-//             <label className='ml-2' >About us</label>
-//             </a>
             <a style={{marginLeft:17}} href='/terms' >
             <label className='ml-2' >Terms and Privacy Policy</label>
             </a>
-      
+
             {/* <NavLink activeClassName="active" to="https://policywrite.s3.ap-south-1.amazonaws.com/Doc1.docx">Login</NavLink> */}
 
 
